bef9ec251cc03632aea3c37901e494ce691789ec
Rubén Medellín <ruben.medellin.c@gmail.com>
2020-08-17 21:25:22 -0500

    Fixed typo in Pets name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
-const Pet = require('./src/models/pets')
+const Pets = require('./src/models/pets')
 
 app.listen(port)
 
